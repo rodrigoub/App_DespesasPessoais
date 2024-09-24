0e4c81cc99187503f8241891b6de12ea582e1e02
Rodrigo Bastos <rodrigoub79@gmail.com>
2024-09-24 16:52:59 -0300

    Criação e instância da classe (transactions) / Mapear dados para Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,63 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
-void main () {
+void main() {
   runApp(ExpensesApp());
-} 
-  
-class ExpensesApp extends StatelessWidget{
+}
 
-@override
-  Widget build(BuildContext Context)  {
-    
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext Context) {
     return MaterialApp(
-   home: MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
+  final _transaction = [
+    Transaction(
+        id: 't1',
+        title: 'novo tênis de corrida',
+        value: 310.90,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Compra de café',
+      value: 15.0,
+      date: DateTime(2024, 9, 24),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Despesas Pessoais'),
-        ),
-        backgroundColor: const Color.fromARGB(255, 154, 14, 179),
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Gráfico'),
-              elevation: 5,
-            ),
+        appBar: AppBar(
+          title: Center(
+            child: Text('Despesas Pessoais'),
           ),
-          Card(
-            child: Text('Lista de Transações'),   
-          )
-        ],
-      )
-    );
+          backgroundColor: const Color.fromARGB(255, 154, 14, 179),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Column(children: <Widget>[
+              ..._transaction.map((tr) {
+                return Card(
+                  child: Text(tr.title),
+                );
+              })
+            ]
+            )
+          ],
+        )
+     );
   }
 }
