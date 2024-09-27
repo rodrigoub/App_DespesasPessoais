2299f5cf46b4cf43404ea7fdce8dec189cd0aa79
Rodrigo Bastos <rodrigoub79@gmail.com>
2024-09-27 18:47:31 -0300

    Criação de Intem Lista Personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -51,13 +51,22 @@ class MyHomePage extends StatelessWidget {
             Column(children: <Widget>[
               ..._transaction.map((tr) {
                 return Card(
-                  child: Text(tr.title),
-                );
+                    child: Row(
+                    children: [
+                    Container(
+                    child: Text(tr.value.toString()),
+                  ),
+                  Column(
+                    children: [
+                      Text(tr.title),
+                      Text(tr.date.toString())
+                    ],
+                  )
+                  ],
+                ));
               })
-            ]
-            )
+            ])
           ],
-        )
-     );
+        ));
   }
 }
